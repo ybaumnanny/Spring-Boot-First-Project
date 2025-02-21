9fcbd55c5bda461f873d5a3633d3cb01fa1b1d1b
Yaman Mahtha <yamanmahtha@gmail.com>
2025-02-21 15:26:41 +0530

    [Yaman] add. Hello Mark with BridgeLabz Done

--- src/main/java/com/example/BridgeLabz/HelloController.java
+++ src/main/java/com/example/BridgeLabz/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.BridgeLabz;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+//    UC1-Hello_From_BridgeLabz
     @RequestMapping("/hello")
     public String sayHello(){
         return "Hello from BridgeLabz.";
     }
+//    UC2-Hello_Mark_From_BridgeLabz
+    @RequestMapping("/hello/query")
+    public String greetUser(@RequestParam String name){
+        return "Hello " + name + " From BridgeLabz";
+}
+
 
 
 }
