ed657cd9f115adb8f00436786cc1d01864eeed19
Yaman Mahtha <yamanmahtha@gmail.com>
2025-02-21 14:42:35 +0530

    [Yaman] Project Creted

---
+++ src/main/java/com/example/BridgeLabz/BridgeLabzApplication.java
@@ -0,0 +1,13 @@
+package com.example.BridgeLabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeLabzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeLabzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BridgeLabz/BridgeLabzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BridgeLabz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeLabzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
